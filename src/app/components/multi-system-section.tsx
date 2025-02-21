4e9f3a1de06337087b9e717e819ea8ef7dfee507
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-02-21 00:43:28 -0500

    Section selection action

--- src/app/components/multi-system-section.tsx
+++ src/app/components/multi-system-section.tsx
@@ -1,9 +1,14 @@
 import { MultiSystemSection as MultiSystemSectionProps } from "@/app/music-diagram-ast/music-diagram-ast";
 import SystemSegments from "@/app/components/system-segments";
 import { colorMap } from "@/app/colors";
+import clsx from "clsx";
 export default function MultiSystemSection(props: MultiSystemSectionProps) {
   const horizontalPaddingValue = `${props.paddingLevel * 8}px`;
   const color = colorMap[props.attributes.color ?? "gray"];
+
+  function selectSection() {
+    console.log("select section" + props.attributes.name);
+  }
 
   return (
     <div className="col-span-full grid grid-cols-subgrid">
@@ -13,10 +18,12 @@ export default function MultiSystemSection(props: MultiSystemSectionProps) {
           color,
         }}
       >
-        {props.attributes.name}
+        <span className="cursor-pointer" onClick={selectSection}>
+          {props.attributes.name}
+        </span>
       </h2>
       <div
-        className="col-span-full grid grid-cols-subgrid gap-y-3 rounded-xl border-2 px-2 py-3"
+        className="relative col-span-full grid grid-cols-subgrid gap-y-3 rounded-xl border-2 px-2 py-3"
         style={{
           paddingLeft: horizontalPaddingValue,
           paddingRight: horizontalPaddingValue,
@@ -25,6 +32,19 @@ export default function MultiSystemSection(props: MultiSystemSectionProps) {
           borderColor: color,
         }}
       >
+        {[
+          "inset-x-0 h-4 -top-2",
+          "inset-x-0 h-4 -bottom-2",
+          "inset-y-0 w-4 -left-2",
+          "inset-y-0 w-4 -right-2",
+        ].map((positionClasses) => {
+          return (
+            <div
+              className={clsx("absolute cursor-pointer", positionClasses)}
+              onClick={selectSection}
+            />
+          );
+        })}
         <SystemSegments segments={props.segments} />
       </div>
     </div>
