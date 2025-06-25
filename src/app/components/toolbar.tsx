b18ca8caa66d6549bbe460d45f99115d1ec94dc6
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-06-25 17:13:20 -0400

    Simplify toolbar code

--- src/app/components/toolbar.tsx
+++ src/app/components/toolbar.tsx
@@ -11,17 +11,13 @@ export function Toolbar() {
     <div className="bg-gray-50 border-b border-gray-200">
       <div className="max-w-7xl mx-auto max-sm:px-6">
         <div className="flex items-center gap-1 py-1">
-          <div className="flex items-center gap-1">
-            <ToolButton action={undo} />
-            <ToolButton action={redo} />
-          </div>
+          <ToolButton action={undo} />
+          <ToolButton action={redo} />
 
           <div className="w-px h-6 bg-gray-300 mx-2" />
 
-          <div className="flex items-center gap-1">
-            <ToolButton action={addBars} />
-            <ToolButton action={deleteSelected} />
-          </div>
+          <ToolButton action={addBars} />
+          <ToolButton action={deleteSelected} />
         </div>
       </div>
     </div>
