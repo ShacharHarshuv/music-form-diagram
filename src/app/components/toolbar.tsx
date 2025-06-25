a447f69e52c8333acd3edbc0d511f30b845ac4db
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-06-25 18:54:57 -0400

    Comment out bar move actions

--- src/app/components/toolbar.tsx
+++ src/app/components/toolbar.tsx
@@ -3,7 +3,6 @@
 import { addBars } from "@/app/actions/add-bars";
 import { createSection } from "@/app/actions/create-section";
 import { deleteSelected } from "@/app/actions/delete";
-import { moveBarsActions } from "@/app/actions/move-bars";
 import { newFile } from "@/app/actions/new";
 import { open } from "@/app/actions/open";
 import { redo } from "@/app/actions/redo";
@@ -27,26 +26,18 @@ export function Toolbar() {
     <div className="bg-gray-50 border-b border-gray-200">
       <div className="max-w-7xl mx-auto max-sm:px-6">
         <div className="flex items-center gap-1 py-1">
-          {/* File operations */}
-          {/* TODO: update icon to be sheet with star */}
-          <ToolButton action={newFile} />{" "}
-          {/* TODO: update to be an open folder icon */}
-          <ToolButton action={open} />{" "}
-          {/* TODO: update save to be floppy disk */}
+          <ToolButton action={newFile} /> <ToolButton action={open} />{" "}
           <ToolButton action={save} />{" "}
           <div className="w-px h-6 bg-gray-300 mx-2" />
-          {/* History */}
           <ToolButton action={undo} />
           <ToolButton action={redo} />
           <div className="w-px h-6 bg-gray-300 mx-2" />
-          {/* Content operations */}
           <ToolButton action={addBars} ref={addButton} />
           <ToolButton action={deleteSelected} />
-          {/* TODO: we can create a better icon probably */}
           <ToolButton action={createSection} />
-          {moveBarsActions.map((action) => (
+          {/* {moveBarsActions.map((action) => (
             <ToolButton key={action.description} action={action} />
-          ))}
+          ))} */}
           <ToolButton action={addNotes} />
         </div>
       </div>
