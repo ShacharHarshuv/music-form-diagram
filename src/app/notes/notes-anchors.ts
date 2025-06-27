b058af7ba9844abf468c6e6a2472c058344245e5
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-06-27 17:58:34 -0400

    add logs

--- src/app/notes/notes-anchors.ts
+++ src/app/notes/notes-anchors.ts
@@ -5,15 +5,18 @@ const subscribers = new Set<() => void>();
 
 export const NotesAnchors = {
   register: (id: string, element: HTMLElement | null) => {
+    console.log("NotesAnchors.register", id, element);
     anchors[id] = element;
     subscribers.forEach((callback) => callback());
   },
   unregister: (id: string) => {
+    console.log("NotesAnchors.unregister", id);
     delete anchors[id];
     subscribers.forEach((callback) => callback());
   },
   useAnchors() {
     const [anchorState, setAnchorState] = useState(anchors);
+    console.log("NotesAnchors.useAnchors re-render", anchorState);
 
     useEffect(() => {
       const subscription = () => {
