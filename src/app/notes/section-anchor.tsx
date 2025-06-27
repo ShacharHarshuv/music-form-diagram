58d9493b462ebefd1a50a631fd7e4cb3ed8d2178
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-06-27 17:56:18 -0400

    add more logs

--- src/app/notes/section-anchor.tsx
+++ src/app/notes/section-anchor.tsx
@@ -2,9 +2,12 @@ import { useEffect, useRef } from "react";
 import { NotesAnchors } from "./notes-anchors";
 
 export function useNoteAnchor<Element extends HTMLElement>(id: string) {
+  console.log("useNoteAnchor", id);
+
   const elementRef = useRef<Element | null>(null);
 
   useEffect(() => {
+    console.log("useNoteAnchor:useEffect", id, elementRef.current);
     if (!elementRef.current) {
       return;
     }
